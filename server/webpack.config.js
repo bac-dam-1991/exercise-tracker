ba9b0e19c9bbb425eb6e634401a63559e47e7a61
bac-dam-1991 <66862347+bac-dam-1991@users.noreply.github.com>
2022-01-25 23:39:29 +1100

    Say hello pipeline. (#6)

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const isProd = process.env.NODE_ENV === "production";
+const path = require('path');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: isProd ? "production" : "development",
+  mode: isProd ? 'production' : 'development',
   entry: {
-    path: path.join(__dirname, "src", "index.ts"),
+    path: path.join(__dirname, 'src', 'index.ts'),
   },
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
-  target: "node",
+  target: 'node',
   module: {
     rules: [
-      { test: /\.js$/, use: ["babel-loader"], exclude: /node_modules/ },
-      { test: /\.ts$/, use: ["ts-loader"], exclude: /node_modules/ },
+      {test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/},
+      {test: /\.ts$/, use: ['ts-loader'], exclude: /node_modules/},
     ],
   },
 };
