593fafacfeaa07a56c55283cfa5f636ccaa3cee3
Bac Trach Dam <bac.dam.1991@gmail.com>
2022-01-26 00:46:12 +1100

    Added typescript.

--- client/cypress/integration/exercises/addNewExercise.spec.js
+++ client/cypress/integration/exercises/addNewExercise.spec.js
@@ -7,6 +7,8 @@ describe('Exercise', () => {
       cy.log(exerciseData);
     });
     cy.visit('/');
+  });
+  it('should add exercise successfully', () => {
     cy.intercept(
       {
         method: 'GET',
@@ -19,8 +21,6 @@ describe('Exercise', () => {
     cy.findByText(/Exercise list/i);
     cy.findByTestId('AddCircleOutlineRoundedIcon').click();
     cy.location('pathname').should('include', '/exercises/add');
-  });
-  it('should add exercise successfully', () => {
     cy.findByLabelText('Name').clear().type(cy.exerciseData.name);
     cy.findByRole('button', {name: /Save/i}).click();
     cy.intercept(
@@ -39,15 +39,51 @@ describe('Exercise', () => {
     cy.findByText(/Push-ups/i);
   });
   it('should fail to add exercise', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/v1/exercises/',
+      },
+      {statusCode: 200, body: {data: []}}
+    );
+    cy.findByRole('link', {name: /Exercises/i}).click();
+    cy.location('pathname').should('include', '/exercises');
+    cy.findByText(/Exercise list/i);
+    cy.findByTestId('AddCircleOutlineRoundedIcon').click();
+    cy.location('pathname').should('include', '/exercises/add');
     cy.findByRole('button', {name: /Save/i}).click();
     cy.findByText(/Exercise name is required./i);
   });
   it('should fail to add exercise because exercise name is too short.', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/v1/exercises/',
+      },
+      {statusCode: 200, body: {data: []}}
+    );
+    cy.findByRole('link', {name: /Exercises/i}).click();
+    cy.location('pathname').should('include', '/exercises');
+    cy.findByText(/Exercise list/i);
+    cy.findByTestId('AddCircleOutlineRoundedIcon').click();
+    cy.location('pathname').should('include', '/exercises/add');
     cy.findByLabelText('Name').clear().type('Ab');
     cy.findByRole('button', {name: /Save/i}).click();
     cy.findByText(/Exercise name needs at least 3 characters./i);
   });
   it('should fail to add exercise because exercise name is too long.', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/v1/exercises/',
+      },
+      {statusCode: 200, body: {data: []}}
+    );
+    cy.findByRole('link', {name: /Exercises/i}).click();
+    cy.location('pathname').should('include', '/exercises');
+    cy.findByText(/Exercise list/i);
+    cy.findByTestId('AddCircleOutlineRoundedIcon').click();
+    cy.location('pathname').should('include', '/exercises/add');
     cy.findByLabelText('Name').clear().type('abcdefghijklmnopqrstuvwxyz');
     cy.findByRole('button', {name: /Save/i}).click();
     cy.findByText(/Exercise name cannot be more than 20 characters./i);
